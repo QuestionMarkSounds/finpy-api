d692996a460feea9552586ab83b308ff2bd4ee27
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2024-05-26 21:16:33 +0300

    change password

--- finpy_api.py
+++ finpy_api.py
@@ -309,6 +309,49 @@ def registration():
         if 'cursor' in locals():
             cursor.close()
 
+@app.route('/change-password', methods=['POST'])
+def change_password():
+    try:
+        data = request.json
+        new_password = data.get('newPassword')
+        old_password = data.get('oldPassword')
+        email = data.get('email')
+        if not email or not new_password or not old_password :
+            return jsonify({'message': 'Invalid input'}), HTTPStatus.BAD_REQUEST.value
+        cursor = connection.cursor()
+        cursor.execute("SELECT * from flutter_users WHERE email = %s", (email,))
+        connection.commit()
+        result = cursor.fetchone()
+        if result:
+            print("RESULT: ", result)
+            password_hash = result["password"] 
+
+            if check_password_hash(password_hash, old_password+config["ROACH_KING"]):
+                pass
+                
+            else:
+                return jsonify({'message': 'Invalid credentials'}), HTTPStatus.FORBIDDEN.value
+        else:
+            return jsonify({'message': 'Not a registered user'}), HTTPStatus.NOT_FOUND.value
+
+        password = data.get('new_password') + config["ROACH_KING"]
+        password_hash = generate_password_hash(password)
+
+        cursor = connection.cursor()
+        cursor.execute("UPDATE flutter_users SETpassword = %s WHERE email = %s RETURNING *", (password_hash, email))
+        
+        connection.commit()
+        result = cursor.fetchone()
+        del result['password']
+        return jsonify({'result': result}), HTTPStatus.OK.value
+    except Exception as error:
+        print('Error', error)
+        print(traceback.format_exc())
+        return jsonify({'message': 'Internal server error'}), 500
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
     try:
